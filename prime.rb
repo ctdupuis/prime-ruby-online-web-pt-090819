12b61afabcab380103751f021cd927927821b523
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-17 01:31:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
+require 'pry'
 # Add  code here!
+
+def prime?(num)
+  binding.pry
+  res = num / 2 
+  if num > 1 && res == 1 || num > 1 && res.class == Float
+    return true
+  elsif num < 1 || res.class == Integer 
+    return false
+  end
+end
